4ce6170a6a30ddf9a1297b0f4bed1bfb7b763e6c
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2012-03-03 20:15:36 +0100

    Re-calculate overlay position on each select & don't change element attributes

--- ElementContainer.js
+++ ElementContainer.js
@@ -6,34 +6,40 @@
 
 function ElementContainer(ele) {
   this.element = ele;
-  this.oldBorder = ele.style.border;
-  this.oldBgColor = ele.style.background;
-  this.offsets = this.getOffset();
+  //this.oldBorder = ele.style.border;
+  //this.oldBgColor = ele.style.background;
+  //this.offsets = this.getOffset();
   
   this.overlay = document.createElement('div');
   this.overlay.setAttribute('class','xpathElementOverlay');
-  this.overlay.style.left = this.offsets.left+'px';
+  /*this.overlay.style.left = this.offsets.left+'px';
   this.overlay.style.top = this.offsets.top+'px';
   this.overlay.style.width = this.element.offsetWidth+'px';
-  this.overlay.style.height = this.element.offsetHeight+'px';
+  this.overlay.style.height = this.element.offsetHeight+'px';*/
   this.overlay.style.display = 'none';
   document.body.appendChild(this.overlay);
 }
 
 ElementContainer.prototype.highlight = function() {
-  this.element.style.border = 'solid 1px black';
+  //this.element.style.border = 'solid 1px black';
   //this.element.style.background = 'yellow';
   this.overlay.style.display = 'none';
 };
 
 ElementContainer.prototype.select = function() {
-  this.element.style.border = 'solid 1px red';
+  this.offsets = this.getOffset();
+  this.overlay.style.left = this.offsets.left+'px';
+  this.overlay.style.top = this.offsets.top+'px';
+  this.overlay.style.width = this.element.offsetWidth+'px';
+  this.overlay.style.height = this.element.offsetHeight+'px';
+  
+  //this.element.style.border = 'solid 1px red';
   //this.element.style.background = 'red';
   this.overlay.style.display = '';
 };
 
 ElementContainer.prototype.clear = function() {
-  this.element.style.border = this.oldBorder;
+  //this.element.style.border = this.oldBorder;
   //this.element.style.background = this.oldBgColor;
   document.body.removeChild(this.overlay);
 };
